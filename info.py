5ed42c3a6956f2c92c38ac79b406154908a34dbd
vedizzz2 <son491413@gmail.com>
2025-04-25 07:51:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Cluster0Collection')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1002541390598))
 LAZY_GROUP_LOGS = int(environ.get('LAZY_GROUP_LOGS', -1002343997444))
 REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
-PRIME_MEMBERS_LOGS = int(environ.get('-1002541390598'))
+PRIME_MEMBERS_LOGS = int(environ.get('-1002599639831'))
 
 # PREMIUM ACCESS *
 lazydownloaders = [int(lazydownloaders) if id_pattern.search(lazydownloaders) else lazydownloaders for lazydownloaders in environ.get('PRIME_DOWNLOADERS', '5906206984').split()]
